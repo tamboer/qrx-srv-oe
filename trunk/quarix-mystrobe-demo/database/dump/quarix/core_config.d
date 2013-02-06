154a21ad4cc921145d5ea9a5f26249d7dbf4e8d5
sebestyenszaby@gmail.com <sebestyenszaby@gmail.com@3116626f-d023-7b30-9b31-707aed388697>
2013-02-06 09:31:00 +0000

    Settings for the demo application in the quarix database.

---
+++ trunk/quarix-mystrobe-demo/database/dump/quarix/core_config.d
@@ -0,0 +1,16 @@
+"94a17e14-c7c9-3697-e211-18490db69870" "Application" "" "DateFormat" "dmy"
+"94a17e14-c7c9-3697-e211-18490db69870" "Application" "" "Locale" "RO_RO"
+"94a17e14-c7c9-3697-e211-18490db69870" "Application" "" "LogicalFormat" "yes/no"
+"94a17e14-c7c9-3697-e211-18490db69870" "Application" "" "NumericFormat" "European"
+.
+PSC
+filename=core_config
+records=0000000000004
+ldbname=quarix
+timestamp=2013/02/06-11:20:36
+numformat=46,44
+dateformat=dmy-1950
+map=NO-MAP
+cpstream=UTF-8
+.
+0000000319
