330dd1561b1a35977319a02cecec67defaae32a9
sebestyenszaby@gmail.com <sebestyenszaby@gmail.com@3116626f-d023-7b30-9b31-707aed388697>
2013-11-11 13:47:34 +0000

    Added new setting for the sample application.

--- trunk/quarix-mystrobe-demo/database/dump/quarix/core_config.d
+++ trunk/quarix-mystrobe-demo/database/dump/quarix/core_config.d
@@ -2,15 +2,16 @@
 "94a17e14-c7c9-3697-e211-18490db69870" "Application" "" "Locale" "RO_RO"
 "94a17e14-c7c9-3697-e211-18490db69870" "Application" "" "LogicalFormat" "yes/no"
 "94a17e14-c7c9-3697-e211-18490db69870" "Application" "" "NumericFormat" "European"
+"94a17e14-c7c9-3697-e211-18490db69870" "Application" "Service" "Logging" "com.quarix.service.logging.FileLogger"
 .
 PSC
 filename=core_config
-records=0000000000004
+records=0000000000005
 ldbname=quarix
-timestamp=2013/02/06-11:20:36
+timestamp=2013/11/11-15:44:29
 numformat=46,44
 dateformat=dmy-1950
 map=NO-MAP
 cpstream=UTF-8
 .
-0000000319
+0000000433
